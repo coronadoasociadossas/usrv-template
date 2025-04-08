b621d0e3bcb55f3c6a40d01173871f48a9ad7882
Wil <mreyeswilson@gmail.com>
2025-04-08 18:53:53 -0500

    first commit

---
+++ src/handlers/hello.ts
@@ -0,0 +1,11 @@
+
+
+export const handler = async () => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello from Lambda!',
+            timestamp: new Date().toISOString(),
+        }),
+    }
+}
